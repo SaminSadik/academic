17d84698181e5c505111a3679cd6196bb85e19cf
Samin Sadik Khan <124756396+SaminSadik@users.noreply.github.com>
2023-02-15 00:10:56 +0600

    Create simpleCalculator.cpp

---
+++ simpleCalculator.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "~~~~~~~~~<< Simple  Calculator >>~~~~~~~~~\n";
+    cout << "~{Input Format: number1 operator number2}~\n";
+    cout << "~~~{Valid operators: + , - , * , / , %}~~~\n";
+    cout<<"~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~\n\n";
+    
+    double x, y; char c;
+    cin >> x >> c >> y;
+
+    if(c=='+') cout << "\nAnswer : " << fixed << setprecision(2) << x+y << endl;
+    
+    if(c=='-') cout << "\nAnswer : " << fixed << setprecision(2) << x-y << endl;
+    
+    if(c=='*') cout << "\nAnswer : " << fixed << setprecision(2) << x*y << endl;
+    
+    if(c=='/') cout << "\nAnswer : " << fixed << setprecision(2) << x/y << endl;
+    
+    if(c=='%')
+    {
+        int a = x, b = y;
+        if(x-a != 0 || y-b != 0) cout << "\n(Fractional values are ignored)\n";
+        cout << "\nAnswer : " << a % b << endl;
+    }
+
+    cout << "\n~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~\n";
+
+    return 0;
+}
